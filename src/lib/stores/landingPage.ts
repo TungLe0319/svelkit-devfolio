5fe2d6b8dc8547568a130d512b16a09286448d4d
Tung N Le <tung.le0319@gmail.com>
2023-10-22 23:12:36 -0600

    Static Folder

--- src/lib/stores/landingPage.ts
+++ src/lib/stores/landingPage.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const isDarkMode = writable(true)
-export const videoSource = writable('src/lib/assets/animated1.mp4');
+export const videoSource = writable('./animated1.mp4');
 
